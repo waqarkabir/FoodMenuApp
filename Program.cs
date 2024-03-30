1f2057fb343d91dcf9487a1ae0fc381801824823
Waqar Kabir <waqarkabi@hotmail.com>
2024-03-30 13:47:23 +0500

    Auto Updating db for migration: added a comment

--- Program.cs
+++ Program.cs
@@ -10,19 +10,31 @@ namespace FoodMenuApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddDbContext<MenuContext>(options => 
+            builder.Services.AddDbContext<MenuContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("MenuDb")));
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+
+
+            if (app.Environment.IsDevelopment())
             {
+                //For Auto updating migration to db
+
+                //using (var scope = app.Services.CreateScope())
+                //{
+                //    var context = scope.ServiceProvider.GetRequiredService<MenuContext>();
+                //    context.Database.Migrate();
+                //}
+            }
+            else
+            {
+
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-
             }
 
             app.UseHttpsRedirection();
